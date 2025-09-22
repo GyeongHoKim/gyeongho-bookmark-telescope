b0a8977949b4958e35ecd768703db1cb6f6895b2
GyeongHoKim <rlarudgh2017@gmail.com>
2025-09-22 10:23:56 +0900

    chore: added test workflows
    - test coverage (vitest coverage option)
    - test results in junit xml (added .gitignore)

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,5 +9,21 @@ export default defineConfig({
     setupFiles: './test/setup.ts',
     globals: true,
     include: ['test/**/*.test.{ts,tsx}', 'test/**/*.spec.{ts,tsx}'],
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './test-results.xml',
+    },
+    coverage: {
+      reporter: ['text', 'json', 'json-summary', 'html'],
+      exclude: [
+        'node_modules/',
+        'test/',
+        'dist/',
+        'coverage/',
+        '**/*.d.ts',
+        'wxt.config.ts',
+        'vitest.config.ts',
+      ],
+    },
   },
 });
